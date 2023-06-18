c6abfe94dab8c6833381e0d70df138f3aa5b5478
Ivann Schlosser <ivann.schlosser.19@ucl.ac.uk>
2023-06-18 14:07:20 +0200

    First commit of website down by hand from scratch

---
+++ interaction.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+    
+    var interests = ['gis','spatial_data','urban_analytics','modelling','cities','circular_economy','sustainability'];
+    var also_interests = ['physics', 'science', 'music', 'photography', 'architecture', 'cooking'];
+    var interests_ = $('#interests');
+    
+    $.each(interests,function(id,el) {
+        el = '#'+el;
+        // console.log(el);
+        interests_.append('<code>'+el+'</code>');
+    });
+  });
+
+
+
+
